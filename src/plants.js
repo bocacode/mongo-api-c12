d6b10c6278b36d70f76d702b8654227df8fb4193
Todd H. Albert <todd@bocacode.com>
2023-10-18 11:29:13 -0400

    create and add working

---
+++ src/plants.js
@@ -0,0 +1,22 @@
+import { db } from './connectDb.js'
+
+export function getAllPlants(req, res) {
+  db.collection('plants').find({}).toArray()
+    .then(plants => {
+      res.send(plants)
+    })
+    .catch(err => {
+      res.status(500).send({ success: false, message: err })
+    })
+}
+
+export function addPlant(req, res) {
+  const newPlant = req.body
+  db.collection('plants').insertOne(newPlant)
+    .then(() => {
+      res.status(201).send({ message: "Plant added", success: true })
+    })
+    .catch(err => {
+      res.status(500).send({ success: false, message: err })
+    })
+}
---
+++ src/secrets.js
@@ -0,0 +1,2 @@
+
+export const connectionString = "mongodb+srv://toddalbert:V4XbVjhD5UQJYNrR@myfirstcluster.vjvlixp.mongodb.net/?retryWrites=true&w=majority"
